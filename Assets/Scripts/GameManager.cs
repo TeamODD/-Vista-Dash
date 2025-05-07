78949147816e1336f5207ba6d1c76aab0235b509
seongsoon1818 <ykdl0327@gmail.com>
2025-05-08 00:15:42 +0900

    0508_0015
    환경 구성

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Merge_Test1 : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
