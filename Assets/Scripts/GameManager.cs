d8f5ded4cdf87b19fa01855e502a368a9cf7827f
seongsoon1818 <ykdl0327@gmail.com>
2025-05-08 17:46:19 +0900

    0508_1745 GameManager, PlatformSpawner
    GameManager, PlatformSpawner 변경
    ScrollingObject 조금 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,22 +2,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public PlatformSpawner Spawner;
-    public int CurrentStage = 1;
+    public PlatformSpawner Spawner; //PlatformSpawner 를 Spawner로 불러옴
+    public ScrollingObject ScrollingObject;
+
+    public int CurrentStage = 1; //현재 스테이지값 = 1
+
+    public float Stage2Multiple = 1.2f;
+    public float Stage3Multiple = 1.5f;
+
+    public float CurrentSpeed = 10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Spawner.UpdatePlatformSpeed(CurrentSpeed); // PlatformSpawner의 Speed를 현재 속도로 변경
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        int CurrentScore = Spawner.GetScore();
+        int CurrentScore = Spawner.GetScore(); // PlatformSpawner에서 현재 스코어 받아옴
 
-        if (CurrentScore >= CurrentStage * 10) 
+        if (CurrentStage == 1 && CurrentScore >= 10) // 2스테이지 진입시
         {
-            CurrentStage++;
+            CurrentStage = 2; 
+            CurrentSpeed = CurrentSpeed * Stage2Multiple; // 1.2배 == 12f
+            Spawner.UpdatePlatformSpeed(CurrentSpeed); // 스포너
+            ScrollingObject.UpdateSpeed(CurrentSpeed); // 스크롤링 둘다 속도 변경
+        }
+        else if (CurrentStage == 2 && CurrentScore >= 20) // 3스테이지 진입시
+        {
+            CurrentStage = 3;
+            CurrentSpeed = CurrentSpeed * Stage3Multiple; // 1.5배 == 18f
+            Spawner.UpdatePlatformSpeed(CurrentSpeed);
+            ScrollingObject.UpdateSpeed(CurrentSpeed);
         }
     }
 }
--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -2,17 +2,23 @@ using UnityEngine;
 
 public class PlatformSpawner : MonoBehaviour
 {
-    public int Score = 0;
+    public int Score = 0; //스코어 초기값 0
     public GameObject PlatformPrefeb;
 
+    public float CurrentSpeed;
     public void SpawnPlatform()
     {
 
     }
 
-    public int GetScore()
+    public int GetScore() //현재 스코어 반환
     {
         return Score;
+    }
+
+    public void UpdatePlatformSpeed(float newspeed)
+    {
+        CurrentSpeed = newspeed;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
