bd6b4120b88814fb06f5a903e26fce94c5ed7d98
seongsoon1818 <ykdl0327@gmail.com>
2025-05-09 13:18:25 +0900

    removeitem 수정
    플레이어는 없어지지 않게

--- Assets/Scripts/RemoveItems.cs
+++ Assets/Scripts/RemoveItems.cs
@@ -4,7 +4,7 @@ public class RemoveItems : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-            if(other.gameObject.tag != "BackGround")
+        if ((other.gameObject.tag != "BackGround") && (other.gameObject.tag != "Player")) 
             {
                 Destroy(other);
             }
